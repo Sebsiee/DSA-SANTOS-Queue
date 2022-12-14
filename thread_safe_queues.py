a062a887c4f20875255b1f68b33d62226159b2bb
unknown <santosbasty4@gmail.com>
2022-12-14 22:13:42 +0800

    Second Commit - Changed match in render to if statement

--- thread_safe_queues.py
+++ thread_safe_queues.py
@@ -13,94 +13,6 @@ from rich.console import Group
 from rich.live import Live
 from rich.panel import Panel
 
-class View:
-    def __init__(self, buffer, producers, consumers):
-        self.buffer = buffer
-        self.producers = producers
-        self.consumers = consumers
-
-    def animate(self):
-        with Live(
-            self.render(), screen=True, refresh_per_second=10
-        ) as live:
-            while True:
-                live.update(self.render())
-
-    def render(self):
-
-        match self.buffer:
-            case PriorityQueue():
-                title = "Priority Queue"
-                products = map(str, reversed(list(self.buffer.queue)))
-            case LifoQueue():
-                title = "Stack"
-                products = list(self.buffer.queue)
-            case Queue():
-                title = "Queue"
-                products = reversed(list(self.buffer.queue))
-            case _:
-                title = products = ""
-
-        rows = [
-            Panel(f"[bold]{title}:[/] {', '.join(products)}", width=82)
-        ]
-        pairs = zip_longest(self.producers, self.consumers)
-        for i, (producer, consumer) in enumerate(pairs, 1):
-            left_panel = self.panel(producer, f"Producer {i}")
-            right_panel = self.panel(consumer, f"Consumer {i}")
-            rows.append(Columns([left_panel, right_panel], width=40))
-        return Group(*rows)
-
-    def panel(self, worker, title):
-        if worker is None:
-            return ""
-        padding = " " * int(29 / 100 * worker.progress)
-        align = Align(
-            padding + worker.state, align="left", vertical="middle"
-        )
-        return Panel(align, height=5, title=title)
-
-QUEUE_TYPES = {
-    "fifo": Queue,
-    "lifo": LifoQueue,
-    "heap": PriorityQueue
-}
-
-def main(args):
-    buffer = QUEUE_TYPES[args.queue]()
-    products = PRIORITIZED_PRODUCTS if args.queue == "heap" else PRODUCTS
-    producers = [
-        Producer(args.producer_speed, buffer, products)
-        for _ in range(args.producers)
-    ]
-    consumers = [
-        Consumer(args.consumer_speed, buffer) for _ in range(args.consumers)
-    ]
-
-    for producer in producers:
-        producer.start()
-
-    for consumer in consumers:
-        consumer.start()
-
-    view = View(buffer, producers, consumers)
-    view.animate()
-
-def parse_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-q", "--queue", choices=QUEUE_TYPES, default="fifo")
-    parser.add_argument("-p", "--producers", type=int, default=3)
-    parser.add_argument("-c", "--consumers", type=int, default=2)
-    parser.add_argument("-ps", "--producer-speed", type=int, default=1)
-    parser.add_argument("-cs", "--consumer-speed", type=int, default=1)
-    return parser.parse_args()
-
-if __name__ == "__main__":
-    try:
-        main(parse_args())
-    except KeyboardInterrupt:
-        pass
-
 PRODUCTS = (
     ":balloon:",
     ":cookie:",
@@ -168,21 +80,49 @@ class Consumer(Worker):
             self.buffer.task_done()
             self.simulate_idle()
 
-@dataclass(order=True)
-class Product:
-    priority: int
-    label: str = field(compare=False)
+class View:
+    def __init__(self, buffer, producers, consumers):
+        self.buffer = buffer
+        self.producers = producers
+        self.consumers = consumers
 
-    def __str__(self):
-        return self.label
+    def animate(self):
+        with Live(
+            self.render(), screen=True, refresh_per_second=10
+        ) as live:
+            while True:
+                live.update(self.render())
 
-class Priority(IntEnum):
-    HIGH = 1
-    MEDIUM = 2
-    LOW = 3
+    def render(self):
 
-PRIORITIZED_PRODUCTS = (
-    Product(Priority.HIGH, ":1st_place_medal:"),
-    Product(Priority.MEDIUM, ":2nd_place_medal:"),
-    Product(Priority.LOW, ":3rd_place_medal:"),
-)
+        if self.buffer == PriorityQueue():
+            title = "Priority Queue"
+            products = map(str, reversed(list(self.buffer.queue)))
+        elif self.buffer == LifoQueue():
+            title = "Stack"
+            products = list(self.buffer.queue)
+        elif self.buffer == Queue():
+            title = "Queue"
+            products = reversed(list(self.buffer.queue))
+        else:
+            title = products = ""
+
+        rows = [
+            Panel(f"[bold]{title}:[/] {', '.join(products)}", width=82)
+        ]
+        pairs = zip_longest(self.producers, self.consumers)
+        for i, (producer, consumer) in enumerate(pairs, 1):
+            left_panel = self.panel(producer, f"Producer {i}")
+            right_panel = self.panel(consumer, f"Consumer {i}")
+            rows.append(Columns([left_panel, right_panel], width=40))
+        return Group(*rows)
+
+    def panel(self, worker, title):
+        if worker is None:
+            return ""
+        padding = " " * int(29 / 100 * worker.progress)
+        align = Align(
+            padding + worker.state, align="left", vertical="middle"
+        )
+        return Panel(align, height=5, title=title)
+
